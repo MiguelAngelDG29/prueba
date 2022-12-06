69b7688e8cb635246864258b408320e8ed64aa31
josetitoarchila <titoarchila@gmail.com>
2022-12-06 10:07:08 -0500

    DOM y JavaScript

--- js/sumar.js
+++ js/sumar.js
@@ -37,3 +37,39 @@ function Sumar(){
 
 
 }
+
+function Sumar2(){
+    let numero1 = document.getElementById("numero1").value;
+    let numero2 = document.getElementById("numero2").value;
+    if(numero1==''){
+        document.getElementById("numero1").setAttribute("class", "form-control is-invalid");
+        document.getElementById("validar_numero1").innerHTML="El numero 1 es obligatorio";
+    }else{
+        document.getElementById("numero1").setAttribute("class", "form-control is-valid");
+        document.getElementById("validar_numero1").innerHTML="";
+    }
+
+    if(numero2==''){
+        document.getElementById("numero2").setAttribute("class", "form-control is-invalid");
+        document.getElementById("validar_numero2").innerHTML="El numero 2 es obligatorio";
+    }else{
+        document.getElementById("numero2").setAttribute("class", "form-control is-valid");
+        document.getElementById("validar_numero2").innerHTML="";
+    }
+    if((numero1!='')&&(numero2!='')){
+        let resultado = Sumar3(parseInt(numero1), parseInt(numero2));
+        
+        //crear una etiqueta o un nodo (etiqueta_h5)
+        let etiqueta_h5 = document.createElement("h5");
+        var contenido_h5= document.createTextNode("el resultado de la suma es "+resultado);
+        etiqueta_h5.appendChild(contenido_h5);
+
+        document.getElementById("caja_resultado").appendChild(etiqueta_h5);
+
+    }
+}
+
+function Sumar3(numero1, numero2){
+    let sumatoria = numero1 + numero2;
+    return sumatoria;
+}
